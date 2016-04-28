297a8d02be96d02f8402490af344754d76e8d24f
Mael Thomas <mael.thomas@data.gouv.fr>
2016-04-28 13:15:34 +0200

    Complete yaml parameters to test
    Two files :
    - parameters.yaml
    - variables_parameters.yaml

--- openfisca_core/taxbenefitsystems.py
+++ openfisca_core/taxbenefitsystems.py
@@ -152,6 +152,6 @@ class MultipleXmlBasedTaxBenefitSystem(AbstractTaxBenefitSystem):
 
         return dict(
             (os.path.basename(path).split('.')[0], yaml.load(file(path, 'r')))
-            for path in self.parameters_yaml_info_list
+            for path in self.yaml_parameters_info_list
         )
 
