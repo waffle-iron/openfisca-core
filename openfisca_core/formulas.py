16800156f96c3a2ef8e25a015c36e610d8d88a49
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-01-16 10:52:33 +0100

    WIP (TO BE REMOVED)

--- openfisca_core/formulas.py
+++ openfisca_core/formulas.py
@@ -409,6 +409,13 @@ class SimpleFormula(AbstractFormula):
             if not isinstance(column, columns.AgeCol):
                 assert not np.isnan(np.sum(array)), u"Function {}@{}<{}> returned nan values : {}".format(
                     column.name, entity.key, str(period), array).encode('utf-8')
+                assert np.isfinite(array).all(), u"Function {}@{}<{}> returned nonfinite values : {}".format(
+                    column.name, entity.key, str(period), array).encode('utf-8')
+                assert (array > - 1e9).all(), u"Function {}@{}<{}> returned values smaller than -1e9: {}".format(
+                    column.name, entity.key, str(period), array).encode('utf-8')
+                assert (array < 1e9).all(), u"Function {}@{}<{}> returned values larger than 1e9: {}".format(
+                    column.name, entity.key, str(period), array).encode('utf-8')
+
         entity_count = entity.count
         assert array.size == entity_count, \
             u"Function {}@{}<{}>() --> <{}>{} returns an array of size {}, but size {} is expected for {}".format(
